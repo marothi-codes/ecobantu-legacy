0227c1f5a174d8d769f15cc8b841b93a872641e6
Marothi Mahlake <code_artist@outlook.com>
2022-08-31 16:00:44 +0200

    Why Us Section Wording Update.

--- src/sections/why-us.js
+++ src/sections/why-us.js
@@ -15,21 +15,21 @@ const data = [
     imgSrc: Partnership,
     altText: 'Our Mission',
     title: 'Our Mission',
-    text: `To be the Premier Entity when it comes to Sustainable Renewable Energy Solutions starting in South Africa, the SADC Region & beyond whilst boasting the highest employee satisfaction and retention in the industry.`,
+    text: `To be the Premier Entity when it comes to Sustainable Renewable Energy Solutions for ALL South Africans with the goal of expanding within the SADC and beyond.`,
   },
   {
     id: 2,
     imgSrc: Performance,
     altText: 'Vision',
     title: 'Our Vision',
-    text: `To democratise Solar Power so that it becomes commonplace thus lowering its barrier to entry so that more civilians can afford it while pioneering better ways of harnessing electricity without the burning of Fossil Fuels.`,
+    text: `To democratise Solar Power so that it becomes commonplace thus lowering the barrier to entry level solar so that more South Africans can afford solar, while pioneering better ways of harnessing electricity without the burning of Fossil Fuels. We want to offer excellent client satisfaction and service, matched with the the highest employee retention in the industry.`,
   },
   {
     id: 3,
     imgSrc: Subscription,
     altText: 'Our Values',
     title: 'Our Values',
-    text: `Our values include but are not limited to: A human-centric working environment, Pragmatic Ingenuity, A child-like thirst for learning, Growth through obstacles, Happy & Satisfied Clients.`,
+    text: `Our values include but are not limited to: A human-centric working environment, pragmatic Ingenuity, growth through obstacles and a child-like thirst for learning. ecoBantu Solutions (Pty) Ltd is a South African BBBEE Level 1 Solar Retailer, Distributor & installer that believes that reliable electricity is not only a necessity but a BASIC HUMAN RIGHT. People are our Passion.`,
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ const data = [
     altText: 'Post Sales Support',
     title: 'Post Sales Support',
     text:
-      'To our existing clients and prospect clients, we are here for you every step of the way. Working with ecoBantu Solutions is not a transaction but a relationship. Give us a shout whenever in need.',
+      'To our existing clients and prospect clients, we are here for you every step of the way. Working with ecoBantu Solutions is not a transaction but a relationship. Give us a call whenever in you are in need.',
   },
 ];
 
